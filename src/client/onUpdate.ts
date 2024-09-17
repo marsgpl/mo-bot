851529066ccf91a84988c685404624007afd5f34
Iurii Belobeev <marsgpl@gmail.com>
2024-09-17 21:25:52 +0500

    updates omg

--- src/client/onUpdate.ts
+++ src/client/onUpdate.ts
@@ -8,12 +8,11 @@ export function onUpdate(this: Client, payload: KeyValue) {
     const func = getString(payload, 'func')
     const [, patchV] = func.match(/patch_version\s*?=\s*?([0-9]+)/i) || []
     const [, retV] =
-        func.match(/return\s*['"]?([^'"\s\}]+)['"]?\s*\}\s*;/i) || []
+        func.match(/return\s*['"]?([^'"\s\}\;]+)['"]?\s*\}\s*;/i) || []
 
     const id = payload.id
 
-    console.log('server sent update:', 'id:', id, 'func:',
-        func.substring(0, 20) + '...' + func.substring(func.length - 20))
+    console.log('server sent update:', 'id:', id, 'func:', func)
     console.log('update patch_version:', patchV)
     console.log('update return:', retV)
 
